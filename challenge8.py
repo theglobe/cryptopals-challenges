c721ba7f83439babcd3e81a2c2eed369ed4e6bef
Jaroslav K <jarikxz@gmail.com>
2020-01-20 01:14:47 +0100

    Comparing block similarity bytewise instead of bitwise

--- challenge8.py
+++ challenge8.py
@@ -2,30 +2,32 @@
 
 import binascii
 
-from challenge6 import find_key_lengths
+def interblock_unique_chars_ratio(text, blocklength):
+    blocks = [text[i:i+blocklength] for i in range(0, len(text), blocklength)]
+    num_unique = 0
+    for index in range(blocklength):
+        chars = [blocks[block_index][index] for block_index in range(len(blocks))]
+        num_unique += len(set(chars))
+    return num_unique/len(text)
 
-f = open('8.txt', 'r')
+if __name__ == "__main__":
+    f = open('8.txt', 'r')
 
-lowest_ed = 32000
-line_nr = 0
-for line in f.read().split('\n'):
-    line_nr += 1
-    ciphertext = binascii.unhexlify(line.strip())
+    lowest_num_unique = 32000
+    line_nr = 0
+    for line in f.readlines():
+        line_nr += 1
+        ciphertext = binascii.unhexlify(line.strip())
+        num_unique = interblock_unique_chars_ratio(ciphertext, 16)
 
-    if len(ciphertext) > 1:
-        # Re-using this is a bit overkill since we only need one of the elements
-        # in the list it returns.
-        average_distances = find_key_lengths(ciphertext)
-
-        # The edit distance of the 16 byte blocks is the 14th element in this list
-        ed = average_distances[14][0]
-        if (ed < lowest_ed):
-            lowest_ed = ed
+        print(line_nr, num_unique)
+        if (num_unique < lowest_num_unique):
+            lowest_num_unique = num_unique
             detected_ecb = ciphertext
             line_nr_ecb = line_nr
 
-print (lowest_ed, line_nr_ecb)
+    blocks = [detected_ecb[i:i+16] for i in range(0, len(detected_ecb), 16)]
+    for block in blocks:
+        print(block)
 
-blocks = [detected_ecb[i:i+16] for i in range(0, len(detected_ecb), 16)]
-for block in blocks:
-    print(block)
+    print (lowest_num_unique, line_nr_ecb)
