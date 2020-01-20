dc94b0fdf5d26ae4e888dc48735a73c47e07df85
Jaroslav K <jarikxz@gmail.com>
2020-01-20 01:12:59 +0100

    Corrected deletion of block elements and refactored

--- challenge6.py
+++ challenge6.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 import base64
-from challenge3 import find_single_byte_xor, score
+from challenge3 import find_single_byte_xor
 from challenge5 import repeating_key_xor
 
 def popcount(num):
@@ -11,23 +11,34 @@ def hamming_distance(bytes1,bytes2):
 	xored = [(a)^(b) for (a,b) in zip(bytes1,bytes2)]
 	return sum([popcount(x) for x in xored])
 
+def block_edit_distance(ciphertext, blocklength):
+	"""
+	Returns the edit distance for blocks of given length
+	"""
+	edit_distances = []
+	blocks = [ciphertext[i:i+blocklength] for i in range(0, len(ciphertext), blocklength)]
+
+	while (len(blocks) >= 2):
+		first = blocks[0]
+		second = blocks[1]
+		del blocks[0]
+		del blocks[0]
+
+		# The second block might be shorter. In that case it determines
+		# the length over which the hamming_distance is calculated
+		ed = hamming_distance(first, second) / len(second)
+		edit_distances.append(ed)
+	edit_distance = sum(edit_distances) / len(edit_distances)
+	return edit_distance
+
 def find_key_lengths(ciphertext):
 	"""
 	Returns a list of tuples (edit_distance, keylength)
 	"""
 	average_distances = []
 	for keysize in range(2,41):
-		edit_distances = []
-		blocks = [ciphertext[i:i+keysize] for i in range(0, len(ciphertext), keysize)]
-		while (len(blocks) > 2):
-			first = blocks[0]
-			second = blocks[1]
-			del blocks[0]
-			del blocks[1]
-
-			ed = hamming_distance(first, second) / keysize
-			edit_distances.append(ed)
-		average_distances.append((sum(edit_distances) / len(edit_distances), keysize))
+		ed = block_edit_distance(ciphertext, keysize)
+		average_distances.append( (ed, keysize) )
 	return average_distances
 
 if __name__ == "__main__":
@@ -56,4 +67,4 @@ if __name__ == "__main__":
 	print (keystr)
 
 	decoded = bytes(repeating_key_xor(ciphertext, keystr))
-	print (decoded.decode('ascii'), score(decoded))
+	print (decoded.decode('ascii'))
